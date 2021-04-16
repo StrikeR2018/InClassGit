4d6c72ca2cf13826f0b0296c4b7ac8b5b9e51387
Mingx <limingx@oregonstate.edu>
2021-04-16 04:48:51 -0700

    final commit: print every number by plus, minus, mul, div in a list

--- calculator.py
+++ calculator.py
@@ -1,6 +1,19 @@
 def calc(a, b):
     sum = a + b
-    print(sum)
-
-
-# calc(4, 5)
+    # print(sum)
+    sub = a - b
+    # print (sub)
+    mul = a * b
+    # print(mul)
+    div = a / b
+    # print (div)
+    arr = [sum, sub, mul, div]
+    # print(sum)
+    # print (sub)
+    # print(mul)
+    # print (div)
+    for x in range(len(arr)):
+        print (arr[x]),
+a = int(input("Enter first number："))
+b = int(input("Enter second number："))
+calc(a, b)
