b937c8f9d0bf535639374faeff56d4d24865f321
Mingx <limingx@oregonstate.edu>
2021-04-16 05:38:58 -0700

    first attemp: skeleton #1

---
+++ password.py
@@ -0,0 +1,9 @@
+import random
+import string
+random.SystemRandom()
+
+def passwordGenerate(num) :
+    return 0
+
+num = int(input("Enter a number for the length of the password："))
+passwordGenerate(num)
