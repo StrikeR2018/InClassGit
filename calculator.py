79514d8e67a2ff284a4130c907c1c77ee631d830
Mingx <limingx@oregonstate.edu>
2021-04-16 04:50:29 -0700

    final commit: delete extra comment

--- calculator.py
+++ calculator.py
@@ -1,17 +1,14 @@
 def calc(a, b):
     sum = a + b
-    # print(sum)
+    
     sub = a - b
-    # print (sub)
+ 
     mul = a * b
-    # print(mul)
+  
     div = a / b
-    # print (div)
+
     arr = [sum, sub, mul, div]
-    # print(sum)
-    # print (sub)
-    # print(mul)
-    # print (div)
+  
     for x in range(len(arr)):
         print (arr[x]),
 a = int(input("Enter first number："))
