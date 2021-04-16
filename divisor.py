2f4c8e6b1643a2990dc031e4b6f980c9585d85e8
Mingx <limingx@oregonstate.edu>
2021-04-16 05:13:36 -0700

    final commit #2

--- divisor.py
+++ divisor.py
@@ -1,7 +1,8 @@
 def printDivisors(n) :
     i = 1
     while i <= n :
-        
+        if (n % i==0) :
+            print (i),
         i = i + 1
          
 a = int(input("Enter a number to find its divisors："))
